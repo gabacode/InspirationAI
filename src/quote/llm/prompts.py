1482fc6ddbc2258cdd4a423938d9df9206a6cbe3
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2023-11-24 15:07:30 +0100

    feat: limit amount of words

--- src/quote/llm/prompts.py
+++ src/quote/llm/prompts.py
@@ -7,9 +7,10 @@ QUOTE_PROMPT = PromptTemplate(
     input_variables=["topic"],
     template=dedent(
         """
-        Use the topic provided to generate an original inspirational quote.
+        Use the topic provided to generate an original, deep inspirational quote.
         It should be brief, concise, and easy to understand.
         It has the potential of changing someone's life.
+        Use maximum 25 words in your quote.
         ---
         Topic: {{ topic }}
         Inspirational Quote:
